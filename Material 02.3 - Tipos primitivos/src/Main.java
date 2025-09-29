be3a788f2afc370991158fb614f98c28228453e7
Luis Castro <luisr.contato@gmail.com>
2025-09-28 23:15:21 -0300

    Add files via upload

---
+++ Material 02.3 - Tipos primitivos/src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
+
+/*
+
+Java possui oito tipos primitivos diferentes: boolean, byte, char, short, int, long, float e double.
+Eles possuem suas próprias características e faixa de valores permitidos:
+
+boolean
+É utilizado para representar valores lógicos, podendo assumir apenas dois valores: true ou false.
+É utilizado em expressões condicionais, loops e outros casos onde se deseja avaliar
+se uma determinada condição é verdadeira ou falsa.
+
+byte
+É utilizado para representar valores numéricos inteiros de 8 bits.
+Ele possui uma faixa de valores de -128 a 127.
+
+char
+É utilizado para representar caracteres individuais.
+Ele pode armazenar qualquer caractere Unicode e é representado por aspas simples ('').
+
+short
+É utilizado para representar valores numéricos inteiros de 16 bits.
+Ele possui uma faixa de valores de -32.768 a 32.767.
+
+int
+É utilizado para representar valores numéricos inteiros de 32 bits.
+É um dos tipos de dados mais utilizados para representar números inteiros em Java
+e possui uma faixa de valores de -2.147.483.648 a 2.147.483.647.
+
+long
+É utilizado para representar valores numéricos inteiros de 64 bits.
+Ele é utilizado para representar valores inteiros muito grandes
+e possui uma faixa de valores de -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807.
+
+float
+É utilizado para representar valores numéricos de ponto flutuante, ou seja,
+valores com casas decimais, sendo que ocupa 32 bits de memória.
+Ele pode representar números decimais com até sete dígitos e tem uma precisão limitada,
+o que significa que ele pode arredondar os números se eles forem muito grandes ou muito pequenos.
+
+double
+É similar o float, entretanto ele ocupa 64 bits de memória e pode representar números decimais com até 15 dígitos.
+
+*/
