c6474d54e2ad665acfe77861deb23112d805d081
Luis Castro <luisr.contato@gmail.com>
2025-12-09 09:24:30 -0300

    Delete Atividade 01.2 - Primeiro programa em Java directory

--- Atividade 01.2 - Primeiro programa em Java/src/PrimeiraClasse.java
+++
@@ -1,9 +0,0 @@
-public class PrimeiraClasse {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-
-        System.out.println();
-
-        System.out.println("Concluí a aula 01 e agora estou mergulhando em Java!");
-    }
-}
