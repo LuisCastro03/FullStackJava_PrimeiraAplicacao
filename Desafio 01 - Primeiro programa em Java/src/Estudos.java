33b567d88da100ba33eef6b422a5fe66d5bbd526
Luis Castro <luisr.contato@gmail.com>
2025-12-09 09:25:13 -0300

    Delete Desafio 01 - Primeiro programa em Java directory

--- Desafio 01 - Primeiro programa em Java/src/Estudos.java
+++
@@ -1,7 +0,0 @@
-//Desafio 3
-
-public class Estudos {
-    public static void main(String[] args) {
-        System.out.println("Classe para imprimir anotações de estudo.");
-    }
-}
--- Desafio 01 - Primeiro programa em Java/src/Soma.java
+++
@@ -1,7 +0,0 @@
-//Desafio 4
-
-public class Soma {
-    public static void main(String[] args) {
-        System.out.println(10 + 5);
-    }
-}
