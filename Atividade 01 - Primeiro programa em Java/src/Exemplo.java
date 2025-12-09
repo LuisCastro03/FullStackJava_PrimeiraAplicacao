a9bb27b3d189895e000d6086cf111a6868071f15
Luis Castro <luisr.contato@gmail.com>
2025-12-09 09:29:08 -0300

    Add files via upload

---
+++ Atividade 01 - Primeiro programa em Java/src/Exemplo.java
@@ -0,0 +1,29 @@
+/*
+
+public class Exemplo {
+
+    public void main(String[] args) {
+        System.println("Oi, meu nome é Alura!");
+    }
+
+}
+
+*/
+
+public class Exemplo {
+    public static void main(String[] args) {
+        System.out.println("Oi, meu nome é Alura!");
+    }
+}
+
+/*
+
+No código comentado, não poderia ser imprimido no terminal por
+conta que a linha 5 não possui a palavra-chave "static" após o
+"public". A palavra-chave "static" serve para criar métodos ou
+variáveis que pertencem somenta a classe que está.
+Na linha 6, não possui ".out" após o "System", o que causará
+um erro de compilação. O ".out" serve para exibir dados no
+console quando executa uma aplicação Java.
+
+*/
