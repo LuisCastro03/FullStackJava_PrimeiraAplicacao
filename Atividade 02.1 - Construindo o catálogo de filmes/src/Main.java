1836e79149a7915f9a55b20c2446d43119a0f03e
Luis Castro <luisr.contato@gmail.com>
2025-12-10 08:06:02 -0300

    Add files via upload

---
+++ Atividade 02.1 - Construindo o catálogo de filmes/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String saudacao = "Olá, meu nome é ";
+        String nome = "Alice ";
+        String continuacao = "e minha idade é ";
+        int idade = 17;
+        System.out.println(saudacao + nome + continuacao + idade);
+    }
+}
+
+/*
+
+O código está correto. É possível concatenar uma String com um
+int e o int se escreve com i minúsculo.
+
+*/
