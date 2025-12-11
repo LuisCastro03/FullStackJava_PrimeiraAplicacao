ed08798ff4189c370a6e7f4c73e797d2aa185470
Luis Castro <luisr.contato@gmail.com>
2025-12-11 10:05:07 -0300

    Add files via upload

---
+++ 02 - Construindo o catálogo de filmes/Desafio 2 - Construindo o catálogo de filmes/src/CalculoValor.java
@@ -0,0 +1,16 @@
+// Desafio 4
+
+public class CalculoValor {
+    public static void main(String[] args) {
+        System.out.println("Hello, World...");
+
+        System.out.println("---");
+
+        double precoProduto = 3.99;
+        int quantidade = 4;
+
+        double valorTotal = precoProduto * quantidade;
+
+        System.out.println("O valor total é: " + valorTotal);
+    }
+}
