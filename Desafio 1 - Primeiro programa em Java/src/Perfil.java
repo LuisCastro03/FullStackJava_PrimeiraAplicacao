fa8438144c21cd27335885db5a4e05609dd63462
Luis Castro <luisr.contato@gmail.com>
2025-12-11 10:18:07 -0300

    Add files via upload

---
+++ Desafio 1 - Primeiro programa em Java/src/Perfil.java
@@ -0,0 +1,11 @@
+//Desafio 1
+
+public class Perfil {
+    public static void main(String[] args) {
+        System.out.println("Olá, Luca!");
+
+        //Desafio 2
+
+        System.out.println("Tudo bem?");
+    }
+}
---
+++ Desafio 1 - Primeiro programa em Java/src/Subtracao.java
@@ -0,0 +1,15 @@
+//Desafio 5
+
+public class Subtracao {
+    public static void main(String[] args) {
+        System.out.println(10 - 5);
+    }
+}
+
+/*
+
+Operador -: Esse operador é utilizado para realizar a
+subtração. No exemplo acima, ele subtrai o valor 5 do
+valor 10, resultando em 5.
+
+*/
