216dc467b798c820064fe1cf64da1972a9ea02ce
Luis Castro <luisr.contato@gmail.com>
2025-12-15 09:15:08 -0300

    Add files via upload

---
+++ Atividade 03.2 - Controlando o fluxo da aplicação/src/Programa.java
@@ -0,0 +1,27 @@
+public class Programa {
+    public static void main(String[] args) {
+        for(int contador = 1; contador <= 10; contador++) {
+            System.out.println(contador);
+        }
+    }
+}
+
+/*
+
+                Alguns significados sobre o código
+
+for(int contador = 1; contador <= 10; contador++) {
+
+for: Inicia um laço de repetição;
+
+int contador = 1;: Declara uma variável inteira chamada "contador"
+e a inicializa com o valor 1;
+
+contador <= 10;: Define a condição para o laço continuar
+executando. O laço continuará enquanto o valor de "contador" for
+menor ou igual a 10;
+
+contador++: Incrementa o valor de "contador" em 1 a cada iteração
+do laço.
+
+*/
